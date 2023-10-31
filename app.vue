7347248c8582a9a5817d0b468c17afb640d0f83b
Maxim Belkin <maximbe@ext.bershka.com>
2023-10-31 10:15:34 +0100

    feat: basics data

--- app.vue
+++ app.vue
@@ -1,5 +1,47 @@
+<script setup>
+import { OptionsData, CompositionData } from './workshop/basics/data'
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="container">
+    <div class="block">
+      <fieldset>
+        <legend>Options Data</legend>
+        <OptionsData />
+      </fieldset>
+      <fieldset>
+        <legend>Composition Data</legend>
+        <CompositionData />
+      </fieldset>
+    </div>
   </div>
 </template>
+
+<style>
+  body {
+    font-family: Roboto Mono, monospace;
+    margin: 0;
+    padding: 2rem;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+    width: 100%;
+    min-height: 100vh;
+  }
+
+  .block {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 2rem;
+    width: 100%;
+  }
+
+  fieldset {
+    flex: 1;
+  }
+</style>
---
+++ workshop/basics/data/Composition.vue
@@ -0,0 +1,10 @@
+<script setup>
+const counter = ref(0)
+</script>
+
+<template>
+  <div>
+    <p>Counter: {{ counter }}</p>
+    <button @click="counter++">Increment</button>
+  </div>
+</template>
